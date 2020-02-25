d78c66b1af5e4d07c857af53dc132b92b8af870c
Nebojsa Lukacev <nebojsalukacev@gmail.com>
2020-02-25 17:56:15 +0100

    Dodat interface hotel

---
+++ Hotel.Model.Common/IHotel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hotel.Model.Common
+{
+    public interface IHotel
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        int NumberOfStars { get; set; }
+    }
+}
